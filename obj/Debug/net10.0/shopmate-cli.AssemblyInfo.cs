6edea3bcaaf3ce5aea10e08d4f8807079a71af5b
manik-babu <mdmanikbabu48@gmail.com>
2026-01-12 23:01:55 +0600

    Add presentation slide

--- obj/Debug/net10.0/shopmate-cli.AssemblyInfo.cs
+++ obj/Debug/net10.0/shopmate-cli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shopmate-cli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4b80111e3f9a690371673269212609ffe5d463")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e9fe3b55b1b1344deaf05700528bff56c62646")]
 [assembly: System.Reflection.AssemblyProductAttribute("shopmate-cli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shopmate-cli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
